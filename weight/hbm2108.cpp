16d1d864a029761de3a0befeb02ad083e821d49c
andigor <andigor@mail.ru>
2012-08-11 15:25:47 +0300

    added coff for the hbm2108

--- weight/hbm2108.cpp
+++ weight/hbm2108.cpp
@@ -14,7 +14,7 @@ void Hbm2108::readWeight(QVariant & ret, uint & err)
 
     io_device()->write(req);
 
-    const uchar frame_size = 17;
+    const uchar frame_size = 16;
 
     while ( io_device()->bytesAvailable() < frame_size ) {
         yield();
@@ -35,33 +35,33 @@ QByteArray Hbm2108::weightRequestFrame() const
     QByteArray ba;
     ba += "S";
     ba += QByteArray::number(address);
-    ba += ";MSV?;";
+    ba += ";COF5;MSV?;";
 
     return ba;
 }
 
 int Hbm2108::parseWeightFrameAnswer(const QByteArray& ba, uint & err) const
 {
-    if (parseAddress(ba) != address) {
-        //throw WeightFrameExceptionBadAddress();
+/*    if (parseAddress(ba) != address) {
         err = PorterFrameBadAddress; return -1;
-    }
-    const uchar frame_size = 17;
+    } */
+
+    const uchar frame_size = 16;
     if (ba.size() != frame_size) {
         err = PorterFrameCorrupted; return -1;
     }
 
-    QByteArray ret = ba.left(8);
+    QByteArray ret = ba.mid(4, 7);
 
     bool ok = false;
-    float fret = ret.toInt(&ok);
+    float fret = ret.toFloat(&ok);
 
     if (!ok) {
         //throw WeightFrameExceptionCorrupted();
         err = PorterFrameCorrupted; return -1;
     }
 
-    //qDebug () << "getWeight: " << fret;
+    qDebug () << "getWeight: " << fret;
     err = 0;
     return fret;
 }
