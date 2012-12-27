f5f71f7af536099e01197d62d009c90bb5a5dfec
andigor <andigor@mail.ru>
2012-12-27 10:44:13 +0200

    added move constructor and move assignment for rdb-sql database member

--- alho/weighters/common/weighterconf.h
+++ alho/weighters/common/weighterconf.h
@@ -5,7 +5,10 @@
 #include "cardstructs.h"
 
 #include <QVariantMap>
-class MainSequence;
+
+
+#include <boost/rdb/mysql_database.hpp>
+
 
 struct WeighterConf : public QVariantMap
 {
@@ -15,6 +18,8 @@ public:
     QString platform_type;
 
     QSqlDatabase database;
+
+    boost::rdb::mysql::mysql_database db;
 
     StructConf struct_conf;
 
@@ -29,6 +34,33 @@ public:
 
         return iter->value<T>();
     }
+
+    WeighterConf() {}
+
+    WeighterConf(WeighterConf&& other) : QVariantMap(other)
+    {
+        qSwap( material, other.material );
+        qSwap( weighter_name, other.weighter_name);
+        qSwap( platform_type, other.platform_type );
+        qSwap( database, other.database );
+        qSwap( db, other.db);
+        qSwap( struct_conf, other.struct_conf );
+    }
+
+    WeighterConf& operator=(WeighterConf&& other)
+    {
+        QVariantMap::operator =(other);
+
+        material  = std::move(other.material );
+        weighter_name= std::move( other.weighter_name);
+        platform_type= std::move( other.platform_type );
+        database= std::move( other.database );
+        db = std::move( other.db);
+        struct_conf= std::move(other.struct_conf );
+
+        return *this;
+    }
+
 };
 
 #endif // BASEWEIGHTERINSTANTIAION_H
